f8e32016df2f309d84fac238132d06a2806e7737
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2023-08-21 20:10:05 -0300

    Finalizado navbar com o tailwind

--- upperbank/src/app/components/navbar.jsx
+++ upperbank/src/app/components/navbar.jsx
@@ -4,19 +4,24 @@ import Image from 'next/image';
 import logotipo from '../../../assets/logotipoHome.png'
 
 function Navbar() {
-  return <div>
+  return <div className='flex flex-row justify-around'>
     <Image
       src={logotipo}
       alt='logotipo da empresa'
     />
 
-    <Link href="/servicos"><span className='text-slate-950 no-underline'>Serviços</span></Link>
-    <Link href={"/about"} className='headers-text'>Sobre nós</Link>
-
-    <Link href={"/about"} className='headers-text'>Entrar</Link>
-    <Link href={"/registrar"}>
-      <button>Registrar</button>
-    </Link>
+    <div className='w-60 flex justify-around'>
+      <Link href="/servicos" className='mt-4'>Serviços</Link>
+      <Link href={"/about"} className='mt-4'>Sobre nós</Link>
+    </div>
+    
+    <div className='w-60 flex justify-around'>
+      <Link href={"/about"} className='mt-4'>Entrar</Link>
+      <Link href={"/registrar"}>
+        <button className='bg-neutral-900 text-white w-20 h-10 rounded-md mt-2'>Registrar</button>
+      </Link>
+    </div>
+    
 
   </div>;
 }
--- upperbank/src/app/page.tsx
+++ upperbank/src/app/page.tsx
@@ -3,8 +3,16 @@ import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <main>
+    <main className="m-5">
      <Navbar/>
+
+      <div className="">
+        <div>
+          <span>Facilite seu dinheiro com o UpperBank.</span>
+          <div/>
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi suscipit quae eius corrupti eveniet et numquam accusamus illum optio, hic minus fugiat repellat alias distinctio nisi asperiores commodi minima. Et.</span>
+        </div>
+      </div>
     </main>
   )
 }
