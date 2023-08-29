bb3c9f0daec7c33a97197e51766d4e75babf502f
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2023-08-28 21:15:14 -0300

    Adicionado erro de login

--- upperbank/src/app/about/page.jsx
+++ upperbank/src/app/about/page.jsx
@@ -1,17 +1,14 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
+import Router from 'next/router';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import Link from 'next/link';
-import { useState } from 'react';
 
-
-// import { Container } from './styles';
-
-function about() {
-
+function Login() {
   const [usuario, setUsuario] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleUsuarioChange = (e) => {
     setUsuario(e.target.value);
@@ -21,61 +18,71 @@ function about() {
     setPassword(e.target.value);
   };
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     const data = {
       usuario: usuario,
       password: password,
     };
 
-    fetch('http://localhost:8080/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log('Login successful');
-        } else {
-          console.error('Login failed');
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
+    try {
+      const response = await fetch('http://localhost:8080/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
       });
+
+      if (response.ok) {
+        console.log('Login successful');
+        // Redirecionar para a página de boas-vindas ou apropriada após o login.
+        Router.push('/welcome');
+      } else {
+        console.error('Login failed');
+        setError('Login falhou. Verifique suas credenciais.');
+      }
+    } catch (error) {
+      console.error('Erro:', error);
+      setError('Ocorreu um erro ao fazer login. Tente novamente mais tarde.');
+    }
   };
 
-  return <div>
-    <Navbar />
+  return (
+    <div>
+      <Navbar />
 
-    <div className='bg-lime-950 w-full h-[38.5rem] flex justify-center items-center'>
-      <div className='w-[23.75rem] h-[23.75rem] bg-slate-900 p-10 rounded-3xl flex flex-col justify-center items-center'>
-        <span className='text-white'>Login</span>
-        <input
-          className='w-full mt-5 rounded-2xl'
-          placeholder='  Digite seu email'
-          value={usuario}
-          onChange={handleUsuarioChange}
-        />
-        <input
-          className='w-full m-5 rounded-2xl'
-          placeholder='  Digite sua senha'
-          type='password'
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <Link href={"/forgotPassword"}><span className='text-lime-500'>Esqueceu a senha?</span></Link>
-        <button
-          className='m-10 w-40 h-10 bg-lime-500 rounded-2xl'
-          onClick={handleLogin}
-        >Login</button>
+      <div className='bg-lime-950 w-full h-[38.5rem] flex justify-center items-center'>
+        <div className='w-[23.75rem] h-[23.75rem] bg-slate-900 p-10 rounded-3xl flex flex-col justify-center items-center'>
+          <span className='text-white'>Login</span>
+          <input
+            className='w-full mt-5 rounded-2xl'
+            placeholder='Digite seu email'
+            value={usuario}
+            onChange={handleUsuarioChange}
+          />
+          <input
+            className='w-full m-5 rounded-2xl'
+            placeholder='Digite sua senha'
+            type='password'
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <Link href='/forgotPassword'>
+            <span className='text-lime-500'>Esqueceu a senha?</span>
+          </Link>
+          <button
+            className='m-10 w-40 h-10 bg-lime-500 rounded-2xl'
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+          {error && <p className='text-red-500'>{error}</p>}
+        </div>
       </div>
+
+      <Footer />
     </div>
-
-    <Footer />
-
-  </div>;
+  );
 }
 
-export default about;
+export default Login;
